12d3a538a0e02182d96369a0b3a74ab4157862f5
pvsgowtham <pvsg737@gmail.com>
2020-05-20 18:20:36 +0530

    Updates

---
+++ precache-manifest.cfb7c81274262846d47c606f669d41cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "715f64192af20edc8f8452e27f6b4434",
+    "url": "/react-kanban-dashboard/index.html"
+  },
+  {
+    "revision": "fdddfbbdf923f5593679",
+    "url": "/react-kanban-dashboard/static/js/2.4d79777e.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/react-kanban-dashboard/static/js/2.4d79777e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccaf70c9310dd1da4072",
+    "url": "/react-kanban-dashboard/static/js/main.34800554.chunk.js"
+  },
+  {
+    "revision": "125c81e17c3534e68327",
+    "url": "/react-kanban-dashboard/static/js/runtime-main.fd93a57a.js"
+  }
+]);
