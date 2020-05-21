ca1b5b6b7331c3af4eb2913318981cd6eef4fa4a
pvsgowtham <pvsg737@gmail.com>
2020-05-21 20:17:52 +0530

    Updates

---
+++ precache-manifest.4b17efb695e32e980ee103193039ac96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac58fc5a915de7587584a55993389091",
+    "url": "/react-kanban-dashboard/index.html"
+  },
+  {
+    "revision": "312a3e0fe717075f02c2",
+    "url": "/react-kanban-dashboard/static/css/main.430ad672.chunk.css"
+  },
+  {
+    "revision": "fbc8500d0f5831b95419",
+    "url": "/react-kanban-dashboard/static/js/2.4a15df9f.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/react-kanban-dashboard/static/js/2.4a15df9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "312a3e0fe717075f02c2",
+    "url": "/react-kanban-dashboard/static/js/main.ef9cc958.chunk.js"
+  },
+  {
+    "revision": "125c81e17c3534e68327",
+    "url": "/react-kanban-dashboard/static/js/runtime-main.fd93a57a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-kanban-dashboard/precache-manifest.cfb7c81274262846d47c606f669d41cf.js"
+  "/react-kanban-dashboard/precache-manifest.4b17efb695e32e980ee103193039ac96.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-kanban-dashboard/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
